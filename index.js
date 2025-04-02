5825797fff27e3ca0b2f977fba8e75f0d11973e1
JulianGr16 <juliangaray.2116@gmail.com>
2025-04-02 02:04:53 -0300

    realize correcciones en esta rama

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 import express from 'express'
 
 //-1 configurar un puerto
-const app = express()
-
-app.set('port', process.env.PORT || 4000)
-app.listen(app.get('port'), ()=>{
-    console.info('Estoy escuchando el puerto '+app.get('port'))
-})
 
 //-2 configurar middelwares
 
 //-3 configurar las rutas
-// http://localhost:4000/prueba
-app.get('/prueba', (req, res, next)=>{
-    console.log('alguien hizo una solicitud get a la ruta de prueba')
-    res.send('hola mundo desde el backend')
-})
