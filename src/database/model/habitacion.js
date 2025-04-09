07ba1634de172d7e45e8f1dfef7f739eb248b58c
JulianGr16 <juliangaray.2116@gmail.com>
2025-04-09 15:41:14 -0300

    realize el modelado del documento habitacion

--- src/database/model/habitacion.js
+++ src/database/model/habitacion.js
@@ -1,3 +1,36 @@
 import mongoose, {Schema} from "mongoose";
 
-const habitacionSchema = new Schema()
+const habitacionSchema = new Schema({
+    numero:{
+        type: Number,
+        required: true,
+        min: 1,
+        max: 1000,
+        unique: true
+    },
+    tipo:{
+        type: String,
+        required: true,
+        enum: ["Suite Standard", "Suite Junior", "Suite Premium"],
+    },
+    capacidad:{
+        type: Number,
+        required : true,
+        min: 1,
+        max: 4
+    },
+    precioPorNoche:{
+        type: Number,
+        required: true,
+        min: 5000,
+        max: 40000
+    },
+    fecha:{
+        type: String,
+        requed:true
+    },
+    imagen:{
+        type: String,
+        required: true
+    },
+})
