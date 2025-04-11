b2847b9ab0d356441ccfed624e5421d040e5b872
JulianGr16 <juliangaray.2116@gmail.com>
2025-04-11 15:48:55 -0300

    continue con el modelado. deje mensajes en los campos requeridos y patterns para colocar imagenes

--- src/database/model/habitacion.js
+++ src/database/model/habitacion.js
@@ -3,34 +3,47 @@ import mongoose, {Schema} from "mongoose";
 const habitacionSchema = new Schema({
     numero:{
         type: Number,
-        required: true,
+        required:[true, 'el numero de la habitacion es un dato obligatorio'],
         min: 1,
         max: 1000,
         unique: true
     },
     tipo:{
         type: String,
-        required: true,
+        required: [true, 'el tipo de habitacion es un dato obligatorio'],
         enum: ["Suite Standard", "Suite Junior", "Suite Premium"],
     },
     capacidad:{
         type: Number,
-        required : true,
+        required : [true, 'la capacidad de persona es un dato obligatorio'],
         min: 1,
         max: 4
     },
     precioPorNoche:{
         type: Number,
-        required: true,
-        min: 5000,
-        max: 40000
+        required: [true, 'el precio por noche es un dato obligatorio'],
+        min: [5000, 'el precio minimo es $5.000'],
+        max: [30000, 'el precio maximo es $30.000']
     },
     fecha:{
         type: String,
-        requed:true
+        required:[true, 'la fehca disponible es un dato obligatorio'],
+        match: [
+            /^\d{1,2}\/\d{1,2}\/\d{4} a \d{1,2}\/\d{1,2}\/\d{4}$/,
+            'La fecha debe estar en el formato "dd/mm/yyyy a dd/mm/yyyy"'
+        ]
     },
     imagen:{
         type: String,
-        required: true
+        required: [true, 'la imagen es obligatoria'],
+        validate: {
+            validator: (valor) => {
+              return /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/.test(valor);
+            },
+        },
+        reserva: {
+            type: Boolean,
+            default: false,
+        },
     },
 })
