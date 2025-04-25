ef520a1cb429a5c8f99959ce58dee7c5de4a1aca
Franco Pereyra <francopereyra325@gmail.com>
2025-04-25 02:45:53 -0300

    realice la ruta para mostrar las habitaciones creadas, la ruta para crear una habitacion y la ruta para editar las habitaciones

--- src/controllers/habitaciones.controllers.js
+++ src/controllers/habitaciones.controllers.js
@@ -1,18 +1,50 @@
-export const funcionPrueba = (req, res ) => {
-    console.log('alguien hizo una solicitud get a la ruta de prueba')
-    res.send('hola mundo desde el backend')
-}
+import Habitacion from "../database/model/habitacion.js";
 
-export const crearHabitacion = (req, res) =>{
-    try {
-        //extraer la habitacion del body(p request)
-        console.log(req.body)
-        //validar los datos del body
-        //crear un objeto con el modelo de habitacion
-        //guardar el objeto en la DB
-        //enviar una respuesta de cofirmacion al crear la habitacion
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({mensaje:'ocurrio un error, no te pude crear el producto'})
-    }
-}
+Habitacion;
+export const funcionPrueba = (req, res) => {
+  console.log("alguien hizo una solicitud get a la ruta de prueba");
+  res.send("hola mundo desde el backend");
+};
+
+export const crearHabitacion = async (req, res) => {
+  try {
+    //extraer la habitacion del body(p request)
+    //validar los datos del body
+    //crear un objeto con el modelo de habitacion
+    const habitacionNuevo = new Habitacion(req.body);
+    //guardar el objeto en la DB
+    await habitacionNuevo.save();
+    //enviar una respuesta de cofirmacion al crear la habitacion
+    res.status(201).json({ mensaje: "La habitacion fue creada con exito" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "ocurrio un error, no te pude crear la habitacion" });
+  }
+};
+
+export const listarHabitaciones = async (req, res) => {
+  try {
+    //pedir a la DB la coleccion de productos
+    const habitaciones = await Habitacion.find();
+    //enviar una respuesta de cofirmacion al crear la habitacion
+    res.status(200).json(habitaciones);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "ocurrio un error, no te pude crear la habitacion" });
+  }
+};
+
+export const editarHabitaciones = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "ocurrio un error, no te pude editar la habitacion" });
+  }
+};
--- src/database/routes/habitaciones.routes.js
+++ src/database/routes/habitaciones.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { crearHabitacion, funcionPrueba } from "../../controllers/habitaciones.controllers.js";
+import { crearHabitacion, editarHabitaciones, funcionPrueba, listarHabitaciones } from "../../controllers/habitaciones.controllers.js";
 
 
 const router = Router();
 router.route('/prueba').get(funcionPrueba)
-router.route('/habitaciones').post(crearHabitacion)
+router.route('/habitaciones').post(crearHabitacion).get(listarHabitaciones)
+router.route('/habitaciones/:id').put(editarHabitaciones)
 
 export default router
