ad9275b337543cf85e1f890eddbba2cd28d42ac4
Franco Pereyra <francopereyra325@gmail.com>
2025-04-30 22:27:50 -0300

    hice el crear, editar, mostrar, eliminar y que me muestre un producto seleccionado a traves del id

--- src/controllers/habitaciones.controllers.js
+++ src/controllers/habitaciones.controllers.js
@@ -20,7 +20,7 @@ export const crearHabitacion = async (req, res) => {
     console.error(error);
     res
       .status(500)
-      .json({ mensaje: "ocurrio un error, no te pude crear la habitacion" });
+      .json({ mensaje: "ocurrio un error, no se pudo crear la habitacion" });
   }
 };
 
@@ -34,17 +34,65 @@ export const listarHabitaciones = async (req, res) => {
     console.error(error);
     res
       .status(500)
-      .json({ mensaje: "ocurrio un error, no te pude crear la habitacion" });
+      .json({ mensaje: "ocurrio un error, no se pudo mostrar las habitaciones" });
   }
 };
 
 export const editarHabitaciones = async (req, res) => {
   try {
-    
+    console.log(req.body)
+    console.log(req.params.id)
+    //validar los datos del body
+    //buscar si el produco existe
+    const habitacionBuscada = await Habitacion.findById(req.params.id);
+    console.log(habitacionBuscada)
+
+    //en caso de que el producto no exista contesa con un error 404
+    if(!habitacionBuscada){
+      return res.status(404).json({mensaje: "no se encontro la habitacion"})
+    }
+    //si econtre el producto lo edito
+    await Habitacion.findByIdAndUpdate(req.params.id, req.body)
+    //envio respuesta al fontend
+    res.status(200).json({mensaje: "la habitacion fue editada con exito"})
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .json({ mensaje: "ocurrio un error, no te pude editar la habitacion" });
+    res.status(500).json({ mensaje: "ocurrio un error, no se pudo editar la habitacion" });
   }
 };
+
+export const eliminarHabitaciones = async (req, res) => {
+  try{
+    //verificar si el ID es valido
+    const habitacionBuscada = await Habitacion.findById(req.params.id);
+    //si el ID no existe envio un mensaje de error
+    if(!habitacionBuscada){
+      return res.status(404).json({mensaje: "no se encontro la habitacion"})
+    }
+    //si existe la habitacion con el ID lo borro y contesto al frontend
+    await Habitacion.findByIdAndDelete(req.params.id)
+    res.status(200).json({mensaje: "la habitacion fue eliminada con exito"})
+
+  }catch(error){
+    console.error(error);
+    res.status(500).json({ mensaje: "ocurrio un error, no se pudo eliminar la habitacion" });
+  }
+};
+
+
+export const obtenerHabitacion = async (req, res) => {
+  try{
+    //verificar si la habitacion existe
+    const habitacionBuscada = await Habitacion.findById(req.params.id);
+    //si no existe mensaje de error 404
+    if(!habitacionBuscada){
+      return res.status(404).json({mensaje: "no se encontro la habitacion"})
+    }
+    //si existe desvolver el producto
+    res.status(200).json(habitacionBuscada)
+
+  }catch(error){
+    console.error(error);
+    res.status(500).json({ mensaje: "ocurrio un error, no se pudo obtener la habitacion" });
+  }
+}
--- src/database/routes/habitaciones.routes.js
+++ src/database/routes/habitaciones.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { crearHabitacion, editarHabitaciones, funcionPrueba, listarHabitaciones } from "../../controllers/habitaciones.controllers.js";
+import { crearHabitacion, editarHabitaciones, funcionPrueba, listarHabitaciones, eliminarHabitaciones, obtenerHabitacion} from "../../controllers/habitaciones.controllers.js";
 
 
 const router = Router();
 router.route('/prueba').get(funcionPrueba)
 router.route('/habitaciones').post(crearHabitacion).get(listarHabitaciones)
-router.route('/habitaciones/:id').put(editarHabitaciones)
+router.route('/habitaciones/:id').put(editarHabitaciones).delete(eliminarHabitaciones).get(obtenerHabitacion)
 
 export default router
